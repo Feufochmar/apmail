7e0486945308bb4cd5744c184fab6c592f45a99e
Feufochmar <me@feuforeve.fr>
2020-03-27 17:08:59 +0100

    Change deeply the look of the application. Still to do: timelines/collections, activities.

--- render.js
+++ render.js
@@ -4,211 +4,337 @@ const {Message} = require('./src/message.js')
 const {ConnectedUser} = require('./src/connected-user.js')
 
 // For access of elements
-var Elem = function(id) {
+const Elem = function(id) {
   return window.document.getElementById(id)
 }
 
-// UI actions
-var UI = {
-  // Attributes
-  composed_message: new Message(),
-  // Methods
-  // Actor display
-  renderActorTag: function(actor) {
+// Icon list
+const Icons = {
+  'unknown-user': "/img/unknown-user.svg"
+}
+
+// To render elements that cannot be present in index.html from a model element
+const Render = {
+  // Render an actor in audience fields context
+  audienceActor: function(actor) {
     var display = '<section class="actor-display">'
     if (actor.valid) {
-      if (actor.info.icon) {
-        display = display + '<img src="' + actor.info.icon + '" width="32" height="32" /> '
+      var icon = actor.info.icon
+      if (!icon) {
+        icon = Icons['unknown-user']
       }
-      display = display
-        + '<p style="display:inline-block;"><strong>' + actor.displayName() + '</strong> <br/>'
-        + '<a href="' + actor.urls.profile + '">'
-        + actor.address()
-        + '</a></p>'
+      display = display + '<img src="' + actor.info.icon + '" width="32" height="32" /> '
+      + '<p style="display:inline-block;"><strong>' + actor.displayName() + '</strong> <br/>'
+      + '<a href="' + actor.urls.profile + '">'
+      + actor.address()
+      + '</a></p>'
     } else {
-      display = display
-        + '<p style="display:inline-block;">'
-        + '<a href="' + actor.urls.profile + '">'
-        + 'Other actor'
-        + '</a></p>'
+      display = display + '<img src="' + Icons['unknown-user'] + '" width="32" height="32" /> '
+      + '<p style="display:inline-block;">'
+      + '<a href="' + actor.urls.profile + '">'
+      + 'Other actor'
+      + '</a></p>'
     }
     display = display + '</section>'
     return display
   },
-  // Show the page tab
-  showPageTab: function(is_shown) {
-    Elem('tab-bar').style.visibility = is_shown ? 'visible' : 'hidden'
-  },
-  // Action done when refreshing a page
-  refreshPage: {
-    'select-user': function() {
-      UI.showPageTab(false)
-      ConnectedUser.disconnect()
-      Elem('connect-username').value = ''
+  // Raw data conversion
+  rawData: function(rawData) {
+    var str_data = JSON.stringify(rawData, null, 1)
+    var replace_map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      '\'': '&#039;'
+    }
+    str_data = str_data.replace(/[&<>"']/g, x => replace_map[x])
+    return str_data
+  }
+}
+
+// UI actions
+const UI = {
+  // Attributes
+  composed_message: new Message(), // Message used in composition page
+  current_context: 'my-inbox', // By default, show the inbox
+  is_connected: false, // Indicate if the user is connected
+  other_actor: new Actor(), // Other actor to display
+  // Contextual methods
+  refresh_context: {
+    'send-message': function() {
+      UI.updateNav('send-selector')
+      UI.showTimeline(undefined, undefined)
+      if (UI.is_connected) {
+        UI.showPage('send-message', undefined)
+      } else {
+        UI.showPage('select-user', undefined)
+      }
     },
-    'ask-password': function() {
-      UI.showPageTab(false)
-      Elem('connect-password').value = ''
-      Elem('ask-password-user-info').innerHTML = UI.renderActorTag(ConnectedUser.actor)
+    'my-inbox': function() {
+      UI.updateNav('inbox-selector')
+      if (UI.is_connected) {
+        UI.showTimeline(ConnectedUser.actor.urls.inbox, ConnectedUser.tokens.user.access_token)
+        UI.showPage('show-profile', ConnectedUser.actor)
+      } else {
+        UI.showTimeline(undefined, undefined)
+        UI.showPage('select-user', undefined)
+      }
     },
-    'profile': function() {
-      UI.showPageTab(true)
-      UI.updateProfilePage()
+    'my-outbox': function() {
+      UI.updateNav('outbox-selector')
+      if (UI.is_connected) {
+        UI.showTimeline(ConnectedUser.actor.urls.inbox, ConnectedUser.tokens.user.access_token)
+        UI.showPage('show-profile', ConnectedUser.actor)
+      } else {
+        UI.showTimeline(undefined, undefined)
+        UI.showPage('select-user', undefined)
+      }
     },
-    'send': function() {
-      UI.showPageTab(true)
-      UI.composed_message = new Message()
-      UI.updateSendMessagePage()
+    'my-profile': function() {
+      UI.updateNav('profile-selector')
+      UI.showTimeline(undefined, undefined)
+      if (UI.is_connected) {
+        UI.showPage('show-profile', ConnectedUser.actor)
+      } else {
+        UI.showPage('select-user', undefined)
+      }
     },
-    'inbox': function() {
-      UI.showPageTab(true)
-      Elem('inbox-messages-error').innerHTML = ''
-      Elem('inbox-messages').innerHTML = 'Loading inbox...'
-      UI.showTimeline('inbox-messages', ConnectedUser.tokens.user.access_token, ConnectedUser.actor.urls.inbox)
-    },
-    'outbox': function() {
-      UI.showPageTab(true)
-      Elem('outbox-messages-error').innerHTML = ''
-      Elem('outbox-messages').innerHTML = 'Loading outbox...'
-      UI.showTimeline('outbox-messages', ConnectedUser.tokens.user.access_token, ConnectedUser.actor.urls.outbox)
-    },
-    'lookup': function() {
-      UI.showPageTab(true)
-      Elem('lookup-user-error').innerHTML = ''
-      Elem('lookup-user-info').innerHTML = ''
-      Elem('lookup-user-timeline').innerHTML = ''
-      Elem('lookup-user-timeline-error').innerHTML = ''
+    'other-profile': function() {
+      UI.updateNav(undefined)
+      if (UI.other_actor.urls.outbox) {
+        UI.showTimeline(UI.other_actor.urls.outbox, undefined)
+      } else {
+        UI.displayTimelineError('Actor does not have a public outbox.')
+      }
+      UI.showPage('show-profile', UI.other_actor)
     }
   },
-  // Show a given page
-  showPage: function(page) {
-    ['select-user', 'ask-password', 'profile', 'send', 'inbox', 'outbox', 'lookup'].map(x => Elem(x).style.display = 'none')
-    Elem(page).style.display = 'block'
-    UI.refreshPage[page]()
+  // Page refresh methods
+  refresh_page: {
+    'select-user': function(_) {
+      Elem('connect-username').value = ''
+    },
+    'ask-password': function(_) {
+      Elem('connect-password').value = ''
+      var icon = ConnectedUser.actor.info.icon
+      if (!icon) {
+        icon = Icons['unknown-user']
+      }
+      Elem('ask-password-user-icon').innerHTML = '<img src="' + icon + '" width="32" height="32" />'
+      Elem('ask-password-user-display-name').innerText = ConnectedUser.actor.displayName()
+      Elem('ask-password-user-address').href = ConnectedUser.actor.urls.profile
+      Elem('ask-password-user-address').innerText = ConnectedUser.actor.address()
+    },
+    'show-profile': function(actor) {
+      // data contains the actor to display
+      var icon = actor.info.icon
+      if (!icon) {
+        icon = Icons['unknown-user']
+      }
+      Elem('profile-icon').innerHTML = '<img src="' + icon + '" width="96" height="96" />'
+      Elem('profile-display-name').innerText = actor.displayName()
+      Elem('profile-address').href = actor.urls.profile
+      Elem('profile-address').innerText = actor.address()
+      Elem('profile-summary').innerHTML = actor.info.summary
+      Elem('profile-code-source').value = JSON.stringify(actor.raw, null, 1)
+    },
+    'show-message': function(data) {
+    },
+    'send-message': function(_) {
+      Elem('send-message-to-recipient').value = ''
+      Elem('send-message-cc-recipient').value = ''
+      Elem('send-message-public-visibility').value = UI.composed_message.public_visibility
+      Elem('send-message-follower-visibility').value = UI.composed_message.follower_visibility
+      Elem('send-message-subject').value = UI.composed_message.subject
+      Elem('send-message-content').value = UI.composed_message.content
+      // TO/CC
+      Elem('send-message-to').innerHTML = UI.composed_message.to.map(
+        function(element) {
+          return '<li class="actor-display">' + Render.audienceActor(element) + ' <button onclick="UI.removeToRecipient(\'' + element.urls.profile + '\')">×</button></li>'
+        }).join('')
+      Elem('send-message-cc').innerHTML = UI.composed_message.cc.map(
+        function(element) {
+          return '<li class="actor-display">' + Render.audienceActor(element) + ' <button onclick="UI.removeCcRecipient(\'' + element.urls.profile + '\')">×</button></li>'
+        }).join('')
+    }
   },
-  // When the page loads, load the connected user if already connected
+  // Methods
+  // On load, auto-connect
   checkConnection: function() {
+    // Connect
     ConnectedUser.loadFromLocalStorage(
       function(load_ok, failure_message) {
-        if (load_ok) {
-          // TODO: Refresh the access token ?
-          // No need to show the login pages, go directly to send message page
-          UI.showPage('send')
-        } else {
-          // Show the select user page
-          UI.showPage('select-user')
+        UI.onConnectionChange(load_ok)
+        if (failure_message) {
+          UI.displayContentError(failure_message)
         }
       })
   },
-  // When the user enter its address, load the actor representing the user
+  // On connected, show the right page
+  onConnectionChange: function(connected) {
+    UI.is_connected = connected
+    UI.refresh_context[UI.current_context]()
+  },
+  // Display content errors
+  displayContentError: function(message) {
+    Elem('content-error').style.display = 'block'
+    Elem('content-error').innerText = 'Error: ' + message
+  },
+  // Display timeline errors
+  displayTimelineError: function(message) {
+    Elem('timeline-error').style.display = 'block'
+    Elem('timeline-error').innerText = 'Error: ' + message
+  },
+  // Clear error messages
+  clearError: function() {
+    Elem('content-error').style.display = 'none'
+    Elem('content-error').innerText = ''
+    Elem('timeline-error').style.display = 'none'
+    Elem('timeline-error').innerText = ''
+  },
+  // Show a page
+  showPage: function(page, data) {
+    // Clear errors
+    UI.clearError()
+    // Hide all other pages
+    for (const p in UI.refresh_page) {
+      Elem(p).style.display = 'none'
+    }
+    // Show the page
+    Elem(page).style.display = 'block'
+    // Refresh
+    UI.refresh_page[page](data)
+  },
+  // Show the timeline
+  showTimeline: function(url, token) {
+    if (url) {
+      Elem('timeline').style.display = 'block'
+      Elem('timeline-data').innerHTML = 'Loading collection...'
+      // TODO
+    } else {
+      Elem('timeline').style.display = 'none'
+    }
+  },
+  // Update the nav
+  updateNav: function(selected) {
+    // Unselect all options
+    ['send-selector', 'inbox-selector', 'outbox-selector', 'profile-selector'].map(x => Elem(x).checked = false)
+    if (selected) {
+      Elem(selected).checked = true
+    }
+  },
+  // Change context
+  setContext: function(ctx) {
+    UI.current_context = ctx
+    UI.refresh_context[UI.current_context]()
+  },
+  // Lookup actor
+  lookupActor: function() {
+    // Find the actor and change to the 'other-profile' context
+    UI.other_actor = new Actor()
+    UI.other_actor.loadFromNameServerAddress(
+      Elem('lookup-actor').value,
+      function(load_ok, failure_message) {
+        if (load_ok) {
+          UI.setContext('other-profile')
+        } else {
+          UI.displayContentError('Unable to find user (' + failure_message + ')')
+        }
+      })
+  },
+  // Action on the select-user page
   selectUser: function() {
     // Load the actor and go to the ask-password page
     ConnectedUser.actor.loadFromNameServerAddress(
       Elem('connect-username').value,
       function(load_ok, failure_message) {
         if (load_ok) {
-          UI.showPage('ask-password')
+          UI.showPage('ask-password', undefined)
         } else {
-          Elem('select-user-error').innerText = 'Error: ' + failure_message
+          UI.displayContentError(failure_message)
         }
       })
   },
-  // When the user enter its password, get the access tokens and then go to the send page
+  // Action on the ask-password page
   connectUser: function() {
-    // Connect the user and go to the send page
+    // Connect the user
     ConnectedUser.connect(
       Elem('connect-password').value,
       function(load_ok, failure_message) {
-        if (load_ok) {
-          UI.showPage('send')
-        } else {
-          Elem('ask-password-error').innerText = 'Error: ' + failure_message
+        UI.onConnectionChange(load_ok)
+        if (failure_message) {
+          UI.displayContentError(failure_message)
         }
       })
   },
-  updateProfilePage: function() {
-    Elem('profile-info').innerHTML = UI.renderActor(ConnectedUser.actor)
-  },
-  updateSendMessagePage: function() {
-    Elem('send-message-to-recipient').value = ''
-    Elem('send-message-cc-recipient').value = ''
-    Elem('send-message-public-visibility').value = UI.composed_message.public_visibility
-    Elem('send-message-follower-visibility').value = UI.composed_message.follower_visibility
-    Elem('send-message-subject').value = UI.composed_message.subject
-    Elem('send-message-content').value = UI.composed_message.content
-    // TO/CC
-    Elem('send-message-to').innerHTML = UI.composed_message.to.map(
-      function(element) {
-        return '<li class="actor-display">' + UI.renderActorTag(element) + ' <button onclick="UI.removeToRecipient(\'' + element.urls.profile + '\')">×</button></li>'
-      }).join('')
-    Elem('send-message-cc').innerHTML = UI.composed_message.cc.map(
-      function(element) {
-        return '<li class="actor-display">' + UI.renderActorTag(element) + ' <button onclick="UI.removeCcRecipient(\'' + element.urls.profile + '\')">×</button></li>'
-      }).join('')
-    // Errors
-    Elem('send-message-recipient-error').innerHTML = ''
-    Elem('send-error').innerHTML = ''
-  },
+  // Action on the send page
+  // Update visibility
   updateSendVisibility: function() {
     UI.composed_message.setVisibility(Elem('send-message-public-visibility').value, Elem('send-message-follower-visibility').value)
   },
-  updateSendContent: function() {
-    UI.composed_message.setContent(Elem('send-message-subject').value, Elem('send-message-content').value)
-  },
-  // Add to recipient lists
+  // Add in To
   addToRecipient: function() {
-    var actor = new Actor()
+    const actor = new Actor()
     actor.loadFromNameServerAddress(
       Elem('send-message-to-recipient').value,
       function(load_ok, failure_message) {
         if (load_ok) {
           UI.composed_message.addToRecipient(actor)
-          UI.updateSendMessagePage()
+          UI.showPage('send-message', undefined)
         } else {
-          Elem('send-message-recipient-error').innerHTML = 'Unable to find user (' + failure_message + ')'
+          UI.displayContentError('Unable to find user (' + failure_message + ')')
         }
       })
   },
+  // Add in Cc
   addCcRecipient: function() {
-    var actor = new Actor()
+    const actor = new Actor()
     actor.loadFromNameServerAddress(
-      Elem('send-message-cc-recipient').value,
+      Elem('send-message-to-recipient').value,
       function(load_ok, failure_message) {
         if (load_ok) {
           UI.composed_message.addCcRecipient(actor)
-          UI.updateSendMessagePage()
+          UI.showPage('send-message', undefined)
         } else {
-          Elem('send-message-recipient-error').innerHTML = 'Unable to find user (' + failure_message + ')'
+          UI.displayContentError('Unable to find user (' + failure_message + ')')
         }
       })
   },
-  // Remove from recipient lists
+  // Remove from To
   removeToRecipient: function(url_profile) {
     // Don't fetch the actor, only set the profile url used in removal
-    var actor = new Actor()
+    const actor = new Actor()
     actor.urls.profile = url_profile
-    //
     UI.composed_message.removeToRecipient(actor)
-    UI.updateSendMessagePage()
+    UI.showPage('send-message', undefined)
   },
+  // Remove from Cc
   removeCcRecipient: function(url_profile) {
-    var actor = new Actor()
+    // Don't fetch the actor, only set the profile url used in removal
+    const actor = new Actor()
     actor.urls.profile = url_profile
-    //
     UI.composed_message.removeCcRecipient(actor)
-    UI.updateSendMessagePage()
+    UI.showPage('send-message', undefined)
+  },
+  // Update message content
+  updateSendContent: function() {
+    UI.composed_message.setContent(Elem('send-message-subject').value, Elem('send-message-content').value)
   },
   // Send message
   sendMessage: function() {
     UI.composed_message.send(
       function(is_ok, failure_message) {
         if (is_ok) {
-          UI.showPage('send')
+          UI.showPage('send-message', undefined)
         } else {
-          Elem('send-error').innerText = failure_message
+          UI.displayContentError('Error when sending message: ' + failure_message)
         }
       })
-  },
+  }
+
+
+/*
   renderRawData: function(rawData) {
     var str_data = JSON.stringify(rawData, null, 1)
     var replace_map = {
@@ -300,51 +426,6 @@ var UI = {
         Elem(id + '-error').innerText = failure_message
       }
     })
-  },
-  renderActor: function(actor) {
-    var display = '<section>'
-    if (actor.valid) {
-      if (actor.info.icon) {
-        display = display + '<img src="' + actor.info.icon + '" width="96" height="96" /> '
-      }
-      display = display
-      + '<p style="display:inline-block;"><strong>' + actor.displayName() + '</strong> <br/>'
-      + '<a href="' + actor.urls.profile + '">'
-      + actor.address()
-      + '</a></p>'
-      + '<p>' + actor.info.summary + '</p>'
-    } else {
-      display = display
-      + '<p style="display:inline-block;">'
-      + '<a href="' + actor.urls.profile + '">'
-      + 'Other actor'
-      + '</a></p>'
-    }
-    display = display + '</section>'
-    if (actor.raw) {
-      display = display + UI.renderRawData(actor.raw)
-    }
-    return display
-  },
-  lookupUser: function() {
-    Elem('lookup-user-info').innerHTML = ''
-    Elem('lookup-user-timeline').innerHTML = ''
-    Elem('lookup-user-error').innerHTML = ''
-    Elem('lookup-user-timeline-error').innerHTML = ''
-    var actor = new Actor()
-    actor.loadFromNameServerAddress(
-      Elem('lookup-user').value,
-      function(load_ok, failure_message) {
-        if (load_ok) {
-          Elem('lookup-user-info').innerHTML = UI.renderActor(actor)
-          if (actor.urls.outbox) {
-            UI.showTimeline('lookup-user-timeline', undefined, actor.urls.outbox)
-          } else {
-            Elem('lookup-user-timeline-error').innerText = 'User does not have a public outbox.'
-          }
-        } else {
-          Elem('lookup-user-error').innerText = 'Unable to find user (' + failure_message + ')'
-        }
-      })
   }
+  */
 }
